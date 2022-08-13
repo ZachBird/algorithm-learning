279dd937bbf57711dec2e9ac6ba3079e6e2faf5c
ZachBird <546439325@qq.com>
2022-08-13 21:53:27 +0800

    feat: 768.max-chunks-to-make-sorted-ii

---
+++ leet-code/768.max-chunks-to-make-sorted-ii-未做/max-chunks-to-make-sorted-ii.js
@@ -0,0 +1,19 @@
+/**
+ * @param {number[]} arr
+ * @return {number}
+ */
+const maxChunksToSorted = function (arr) {
+  const stack = [];
+  for (const num of arr) {
+    if (stack.length === 0 || num >= stack[stack.length - 1]) {
+      stack.push(num);
+    } else {
+      const mx = stack.pop();
+      while (stack.length && stack[stack.length - 1] > num) {
+        stack.pop();
+      }
+      stack.push(mx);
+    }
+  }
+  return stack.length;
+};
