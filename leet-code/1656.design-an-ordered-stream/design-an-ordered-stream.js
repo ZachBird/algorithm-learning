d6f157925c3c8875c28c8d9bd1b443f41c6da87e
ZachBird <546439325@qq.com>
2022-08-16 13:33:26 +0800

    feat: 1656.design-an-ordered-stream

---
+++ leet-code/1656.design-an-ordered-stream/design-an-ordered-stream.js
@@ -0,0 +1,49 @@
+/**
+ * @param {number} n
+ */
+const OrderedStream = function (n) {
+  // 模拟题
+  // 有序，数组存
+  this.stream = new Array(n).fill(0);
+  // 存个映射，减少查询
+  this.keyIdxMap = new Map();
+  // 指针，相当于 i + 1
+  this.ptr = 1;
+};
+
+/**
+* @param {number} idKey
+* @param {string} value
+* @return {string[]}
+*/
+OrderedStream.prototype.insert = function (idKey, value) {
+  // 先存入
+  this.stream[idKey - 1] = value;
+  // 更新映射
+  this.keyIdxMap.set(idKey, idKey - 1);
+  const ans = [];
+  if (idKey === this.ptr) {
+    while (this.keyIdxMap.has(idKey)) {
+      const curIndex = this.keyIdxMap.get(idKey);
+      ans.push(this.stream[curIndex]);
+      idKey++;
+    }
+    this.ptr = idKey;
+    return ans;
+  } else {
+    return [];
+  }
+};
+
+/**
+* Your OrderedStream object will be instantiated and called as such:
+* var obj = new OrderedStream(n)
+* var param_1 = obj.insert(idKey,value)
+*/
+
+const os = new OrderedStream(5);
+os.insert(3, 'ccccc'); // 插入 (3, "ccccc")，返回 []
+os.insert(1, 'aaaaa'); // 插入 (1, "aaaaa")，返回 ["aaaaa"]
+os.insert(2, 'bbbbb'); // 插入 (2, "bbbbb")，返回 ["bbbbb", "ccccc"]
+os.insert(5, 'eeeee'); // 插入 (5, "eeeee")，返回 []
+os.insert(4, 'ddddd'); // 插入 (4, "ddddd")，返回 ["ddddd", "eeeee"]
