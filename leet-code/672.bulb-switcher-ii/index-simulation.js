d08a9c5d4f8d74771ea0f770769f5f0c2eb5838a
ZachBird <546439325@qq.com>
2022-09-15 22:51:03 +0800

    feat: 672 upgrade comments

--- leet-code/672.bulb-switcher-ii/index-simulation.js
+++ leet-code/672.bulb-switcher-ii/index-simulation.js
@@ -3,38 +3,44 @@
  * @param {number} presses
  * @return {number}
  */
-const flipLights = function (n = 4, presses = 4) {
-  const ans = new Set();
-  // 二进制数的枚举，记一下
-  for (let i = 0; i < 1 << 4; ++i) {
-    const pressArr = new Array(4).fill(0);
-    for (let j = 0; j < 4; ++j) {
-      pressArr[j] = (i >> j) & 1;
+// 模拟 4 个开关的状态
+const flipLights = function (n, presses) {
+  const ans = new Set(); // 需要去重 n = 1 时
+  // 遍历 4 盏灯的所有可能的操作数，因归纳得知，4 盏灯的状态即可代表任意状态数
+  const hexDigtal = 4;
+  // 二进制数遍历
+  for (let i = 0; i < (1 << hexDigtal); ++i) {
+    // 4 个开关的
+    const switcherCount = 4;
+    const switcherStatus = new Array(switcherCount).fill(0);
+    for (let j = 0; j < switcherCount; ++j) {
+      switcherStatus[j] = (i >> j) & 1;
     }
-    // console.log(i, pressArr);
-    const sumPress = pressArr.reduce((sum, num) => (sum += num));
-    // console.log(sumPress, i);
-    // (i + 1) % 4 === 0 && console.log('---');
-    if (sumPress % 2 === presses % 2 && sumPress <= presses) {
-      // 第一个条件是为了确保sum的奇偶性与总按压次数的奇偶性保持一致
-      // 第二个条件是为了保证presses足够大能够达到i这个状态
-      // 两个条件确保了presses能够让4个开关到达i这个状态
-      let status = pressArr[0] ^ pressArr[2] ^ pressArr[3];
+    // 当前开关状态的操作总数
+    const totalPresses = switcherStatus.reduce((sum, num) => (sum += num));
+    // 1. 保证当前遍历到的开关操作次数的奇偶性与要求奇偶性一致
+    // 2. 保证当前遍历到的开关操作次数达到要求操作次数
+    // 满足 1 和 2 才进行操作数的模拟
+    if ((totalPresses & 1) === (presses & 1) && totalPresses <= presses) {
+      // 编号为 6k+1，受按钮 1,3,4 影响；
+      // 编号为 6k+2, 6k+6，受按钮 1,2 影响；
+      // 编号为 6k+3, 6k+5，受按钮 1,3 影响；
+      // 编号为 6k+4，受按钮 1,2,4 影响。
+      let curBulbStatus = (switcherStatus[0] ^ switcherStatus[2] ^ switcherStatus[3]);
       if (n >= 2) {
-        // 左移一位，记录第二盏灯的状态
-        status |= (pressArr[0] ^ pressArr[1]) << 1;
+        // 按位存当前灯的位置
+        curBulbStatus |= (switcherStatus[0] ^ switcherStatus[1]) << 1;
       }
       if (n >= 3) {
-        status |= (pressArr[0] ^ pressArr[2]) << 2;
+        curBulbStatus |= (switcherStatus[0] ^ switcherStatus[2]) << 2;
       }
       if (n >= 4) {
-        status |= (pressArr[0] ^ pressArr[1] ^ pressArr[3]) << 3;
+        curBulbStatus |= (switcherStatus[0] ^ switcherStatus[1] ^ switcherStatus[3]) << 3;
       }
-      ans.add(status);
+      ans.add(curBulbStatus);
     }
   }
-  // console.log(ans);
-  return ans;
+  return ans.size;
 };
 
 flipLights();
