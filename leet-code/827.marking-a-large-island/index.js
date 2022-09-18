dbfc0dc13df63064e36fe740a38da2cd415fb0a6
ZachBird <546439325@qq.com>
2022-09-19 00:11:42 +0800

    feat: 827.marking-a-large-island debugged

--- leet-code/827.marking-a-large-island/index.js
+++ leet-code/827.marking-a-large-island/index.js
@@ -2,10 +2,9 @@
 //  * @param {number[][]} grid
 //  * @return {number}
 //  */
-
 // 几个题解结合起来用标记岛屿 + 合并的方法解
 // 并查集解法：https://leetcode.cn/problems/making-a-large-island/solution/by-ac_oier-1kmp/
-const dirs = [[0, 1], [0, -1], [-1, 0], [0, -1]]; // 用来快速定方向的数组
+const dirs = [[0, 1], [0, -1], [-1, 0], [1, 0]]; // 用来快速定方向的数组
 const largestIsland = function (grid) {
   const iSize = grid.length;
   const iAreaMap = new Map();
@@ -22,17 +21,22 @@ const largestIsland = function (grid) {
       }
     }
   }
-  if (iAreaMap.size === 0) return 0;
+  // 如果没有 岛屿 最大添加一个格子，为 1
+  if (iAreaMap.size === 0) return 1;
   // 遍历海洋格子
   for (let i = 0; i < iSize; ++i) {
     for (let j = 0; j < iSize; ++j) {
       if (grid[i][j] === 0) {
         let connectedAreas = 1;
+        // 防止有岛屿包裹某个海洋格子，就会重复累加面积
+        const connSet = new Set();
         for (const dir of dirs) {
           const x1 = i + dir[0];
           const y1 = j + dir[1];
-          if (!valid(iSize, x1, y1) || grid[x1][y1] === 0) continue;
-          connectedAreas += iAreaMap.get(grid[x1][y1]);
+          if (!valid(iSize, x1, y1) || grid[x1][y1] === 0 || connSet.has(grid[x1][y1])) continue;
+          const curSign = grid[x1][y1];
+          connectedAreas += iAreaMap.get(curSign);
+          connSet.add(curSign);
         }
         ans = Math.max(ans, connectedAreas);
       }
@@ -43,7 +47,7 @@ const largestIsland = function (grid) {
 
 const calcAreas = function (grid, x, y, sign) {
   const iSize = grid.length;
-  let ans = 0;
+  let ans = 1;
   grid[x][y] = sign;
   for (const dir of dirs) {
     const x1 = x + dir[0];
@@ -59,52 +63,4 @@ const valid = function (iSize, x, y) {
   return x >= 0 && x < iSize && y >= 0 && y < iSize;
 };
 
-// const d = [0, -1, 0, 1, 0];
-// const largestIsland = function (grid) {
-//   const n = grid.length; let res = 0;
-//   const tag = new Array(n).fill(0).map(() => new Array(n).fill(0));
-//   const area = new Map();
-//   for (let i = 0; i < n; i++) {
-//     for (let j = 0; j < n; j++) {
-//       if (grid[i][j] === 1 && tag[i][j] === 0) {
-//         const t = i * n + j + 1;
-//         area.set(t, dfs(grid, i, j, tag, t));
-//         res = Math.max(res, area.get(t));
-//       }
-//     }
-//   }
-//   for (let i = 0; i < n; i++) {
-//     for (let j = 0; j < n; j++) {
-//       if (grid[i][j] === 0) {
-//         let z = 1;
-//         const connected = new Set();
-//         for (let k = 0; k < 4; k++) {
-//           const x = i + d[k]; const y = j + d[k + 1];
-//           if (!valid(n, x, y) || tag[x][y] === 0 || connected.has(tag[x][y])) {
-//             continue;
-//           }
-//           z += area.get(tag[x][y]);
-//           connected.add(tag[x][y]);
-//         }
-//         res = Math.max(res, z);
-//       }
-//     }
-//   }
-//   return res;
-// };
-
-// const dfs = (grid, x, y, tag, t) => {
-//   const n = grid.length; let res = 1;
-//   tag[x][y] = t;
-//   for (let i = 0; i < 4; i++) {
-//     const x1 = x + d[i]; const y1 = y + d[i + 1];
-//     if (valid(n, x1, y1) && grid[x1][y1] === 1 && tag[x1][y1] === 0) {
-//       res += dfs(grid, x1, y1, tag, t);
-//     }
-//   }
-//   return res;
-// };
-
-// const valid = (n, x, y) => {
-//   return x >= 0 && x < n && y >= 0 && y < n;
-// };
+console.log(largestIsland([[0, 0], [0, 0]]));
