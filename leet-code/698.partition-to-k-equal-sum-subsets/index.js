3db9b1c75f1d155c08d2baa1b8907562d582c345
ZachBird <546439325@qq.com>
2022-09-20 23:11:20 +0800

    feat: 698-optimize

--- leet-code/698.partition-to-k-equal-sum-subsets/index.js
+++ leet-code/698.partition-to-k-equal-sum-subsets/index.js
@@ -9,21 +9,29 @@ const canPartitionKSubsets = function (nums, k) {
   if (sums % k !== 0) return false;
   // 每个子集需要满足的和
   const target = sums / k;
-  const bucket = new Array(k);
+  const bucket = new Array(k).fill(0);
+  // MARK: 超时优化点 1，nums 降序排，大数先放桶
+  nums.sort((a, b) => b - a);
   return backtrack(nums, 0, bucket, k, target);
 };
 
 const backtrack = function (nums, index, bucket, k, target) {
   // 结束条件：已经处理完所有球
   if (index === nums.length) {
+    // MARK: 其实这个地方不需要判断，因为当 index == num.length 时，所有球已经按要求装入所有桶，所以肯定是一个满足要求的解
+    // 即：每个桶内球的和一定为 target
     // 判断现在桶中的球是否符合要求 -> 路径是否满足要求
-    for (let i = 0; i < k; i++) {
-      if (bucket[i] !== target) return false;
-    }
+    // for (let i = 0; i < k; i++) {
+    //   if (bucket[i] !== target) return false;
+    // }
     return true;
   }
   // 主要处理逻辑，以数字球的视角，去按步骤放入桶
   for (let i = 0; i < k; ++i) {
+    // MARK: 关键剪枝
+    // 如果当前桶和上一个桶内的元素和相等，则跳过
+    // 原因：如果元素和相等，那么 nums[index] 选择上一个桶和选择当前桶可以得到的结果是一致的
+    if (i > 0 && bucket[i] === bucket[i - 1]) continue;
     // 放入之后当前桶的值大于 target ，则选择下一桶
     if (bucket[i] + nums[index] > target) continue;
     // 否则，置入当前桶
