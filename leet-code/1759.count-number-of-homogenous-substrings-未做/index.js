183d16af27d62c1676d36fe74918975d8181ce29
ZachBird <546439325@qq.com>
2022-12-26 20:53:43 +0800

    feat: 1759.count-number-of-homogenous-substrings-未做

---
+++ leet-code/1759.count-number-of-homogenous-substrings-未做/index.js
@@ -0,0 +1,18 @@
+const countHomogenous = function (s) {
+  const MOD = 1000000007;
+  let res = 0;
+  let prev = s[0];
+  let cnt = 0;
+  for (let i = 0; i < s.length; i++) {
+    const c = s[i];
+    if (c === prev) {
+      cnt++;
+    } else {
+      res += (cnt + 1) * cnt / 2;
+      cnt = 1;
+      prev = c;
+    }
+  }
+  res += (cnt + 1) * cnt / 2;
+  return res % MOD;
+};
